6e3ff4dd8177daf5557848f8956472f24c7c492d
bonino97 <juanbonino97@gmail.com>
2023-10-05 13:17:11 +0200

    Adding routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'pricing',
+    loadChildren: () =>
+      import('./pricing/pricing.module').then((m) => m.PricingModule),
+  },
+  {
+    path: 'contactus',
+    loadChildren: () =>
+      import('./contactus/contactus.module').then((m) => m.ContactusModule),
+  },
+];
